9ad5dba14322568d91c366e11ca22861df8ff5d7
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-11 16:03:27 +0530

    router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 function App() {
   return (
-    <>
-      <h2 className="font-bold">hello</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

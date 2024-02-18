e3eb3c6fd09db8f3c21c1cd477515f41cb721110
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-18 13:26:59 +0530

    create post ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -18,8 +18,9 @@ import {
   signoutSuccess,
   deleteUserSuccess,
 } from "../user/userSlice";
+import { Link } from "react-router-dom";
 const DashProfile = () => {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
@@ -212,6 +213,17 @@ const DashProfile = () => {
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={"/create-post"}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create Post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer" onClick={() => setShowModel(true)}>
